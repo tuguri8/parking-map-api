5fbf8eb11a68052719a0baaeec6a9240f200369c
한승우 <tuguri8@gmail.com>
2019-09-28 18:44:39 +0900

    #1 불필요 테스트 삭제

--- src/main/java/com/bamplee/chomi/api/application/ParkingSyncServiceImpl.java
+++ src/main/java/com/bamplee/chomi/api/application/ParkingSyncServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -39,6 +40,7 @@ public class ParkingSyncServiceImpl implements ParkingSyncService {
         this.parkingInfoRepository = parkingInfoRepository;
     }
 
+    @Transactional
     @Scheduled(cron = "0 0 0/10 * * *")
     @Override
     public void syncParkingInfoList() {
